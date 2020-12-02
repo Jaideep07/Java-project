1e6729a984dec9bbd251c33e5998905598b3fe0f
aswarth123 <c.v.aswarth@gmail.com>
2020-12-02 13:19:40 +0530

    Modified Client.java

--- Java-source-code/src/actorClasses/Client.java
+++ Java-source-code/src/actorClasses/Client.java
@@ -4,7 +4,7 @@ public class Client {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		System.out.println("Hello World");
 	}
 
 }

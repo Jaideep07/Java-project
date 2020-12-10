8de55eda5d22b7a6af6b73c7c638ed27fb4597e5
aswarth123 <c.v.aswarth@gmail.com>
2020-12-10 12:50:06 +0530

    Added Builder.java

--- Java-source-code/src/actorClasses/Builder.java
+++ Java-source-code/src/actorClasses/Builder.java
@@ -1,5 +1,24 @@
 package actorClasses;
 
 public class Builder {
+	
+	public String id;
+	public String jobDescription;
+	public String name;
+	protected String address;
+	public String gender;
+	private double salary;
+	public String Email;
 
+	
+	Builder()
+	{
+		this.id = ""; 
+	}
+	
+	Builder(String id)
+	{
+		
+	}
+	
 }
---
+++ Java-source-code/src/actorClasses/Manager.java
@@ -0,0 +1,10 @@
+package actorClasses;
+
+public class Manager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

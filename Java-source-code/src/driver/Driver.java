d3afa551ad22a3ef7d9605c408af0cd10903cdbb
aswarth123 <c.v.aswarth@gmail.com>
2020-12-14 19:01:06 +0530

    Modified Driver

--- Java-source-code/src/driver/Driver.java
+++ Java-source-code/src/driver/Driver.java
@@ -153,8 +153,8 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//new loginUI();
-		new ManagerGui("B1");
+		new loginUI();
+		
 	}
 
 }
